3624ffb83aafb8cb5fb63f074879e84b7ecf244c
sustainedtang <77056068+sustainedtang@users.noreply.github.com>
2021-06-10 17:15:10 -0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -3,7 +3,7 @@ SELECT * FROM full_21;
 
 SELECT * FROM world_21;
 
--- Join tables on county_id
+-- Join tables on country
 SELECT full_21.country, full_21.year, full_21.total_cases, full_21.total_deaths, world_21.life_expectency, world_21.life_freedom, world_21.corruption_perception
 FROM full_21
 INNER JOIN world_21
